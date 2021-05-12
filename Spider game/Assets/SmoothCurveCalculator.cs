7ab4776f66d95ad28d6d47dac6ff9e4baa38e688
ahg95 <ansgar.glup@gmx.de>
2021-05-12 17:30:41 +0200

    stuff again

---
+++ Spider game/Assets/SmoothCurveCalculator.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCurveCalculator : MonoBehaviour
+{
+    public float DistanceBetweenCurvePoints;
+    public AnimationCurve SmoothingCurve;
+
+    public Vector3[] CalculateSmoothCurvePoints(Vector3[] curvePoints)
+    {
+        // Contains all calculated curve points in order
+        List<Vector3> smoothCurvePointsList = new List<Vector3>();
+
+        // The offset from the startPosition of the currently modeled curve section to the point that should be calculated.
+        float curvePointOffset = 0;
+
+        // We loop until i = dataPoints.Length - 1 because the last dataPoint has already been modeled as the endpoint of the curve
+        for (int i = 0; i < curvePoints.Length - 1; i++)
+        {
+            Vector3 startPosition = curvePoints[i];
+            Vector3 endPosition = curvePoints[i + 1];
+
+            Vector3 startDirection;
+            if (i == 0)
+                startDirection = endPosition - startPosition;
+            else
+                startDirection = endPosition - curvePoints[i - 1];
+
+            Vector3 endDirection;
+            if (i == curvePoints.Length - 2) // This is true if the endPoint is the position of the last dataPoint
+                endDirection = endPosition - startPosition;
+            else
+                endDirection = curvePoints[i + 2] - startPosition;
+
+            float startToEndPointDistance = (endPosition - startPosition).magnitude;
+
+            while (curvePointOffset < startToEndPointDistance)
+            {
+                Vector3 smoothCurvePoint = Vector3Utility.GetPointOfSmoothCurveConnectingTwoPoints(startPosition, startDirection, endPosition, endDirection, curvePointOffset / startToEndPointDistance, SmoothingCurve);
+
+                smoothCurvePointsList.Add(smoothCurvePoint);
+
+                curvePointOffset += DistanceBetweenCurvePoints;
+            }
+            curvePointOffset -= startToEndPointDistance;
+        }
+
+        return smoothCurvePointsList.ToArray();
+    }
+}
---
+++ Spider game/Assets/Vector3Utility.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3Utility
+{
+    /// <summary>
+    /// Models a smooth curve between two given points and returns the point on this curve defined by the provided interpolation value.
+    /// </summary>
+    /// <param name="startPoint">The point where the smooth curve should start.</param>
+    /// <param name="startPointDirection">The direction in which the smooth curve should start.</param>
+    /// <param name="endPoint">The point where the smooth curve should end.</param>
+    /// <param name="endPointDirection">The direction in which the smooth curve should end.</param>
+    /// <param name="t">The interpolation value. If t<=0 then startPoint is returned, if 1<=t then endPoint is returned, and if 0 < t < 1 then an intermediate value on the smooth curve is returned.</param>
+    /// <returns>The point of the defined smooth curve at the given interpolation value.</returns>
+    public static Vector3 GetPointOfSmoothCurveConnectingTwoPoints(Vector3 startPoint, Vector3 startPointDirection, Vector3 endPoint, Vector3 endPointDirection, float t, AnimationCurve smoothingCurve = null)
+    {
+        if (smoothingCurve == null)
+            smoothingCurve = new AnimationCurve();
+
+        Vector3 smoothCurvePoint;
+
+        if (t <= 0)
+            smoothCurvePoint = startPoint;
+        else if (1 <= t)
+            smoothCurvePoint = endPoint;
+        else // 0 < t < 1 holds
+        {
+            float distanceBetweenPoints = (endPoint - startPoint).magnitude;
+
+            startPointDirection = startPointDirection.normalized * distanceBetweenPoints;
+            Vector3 startPointExtension = startPoint + startPointDirection * t;
+
+            endPointDirection = endPointDirection.normalized * distanceBetweenPoints;
+            Vector3 endPointBackwardExtension = endPoint - endPointDirection * (1 - t);
+
+            smoothCurvePoint = Vector3.Lerp(startPointExtension, endPointBackwardExtension, smoothingCurve.Evaluate(t));
+        }
+
+        return smoothCurvePoint;
+    }
+}
