64e1a84a3aa019642dda916f6f99dfdc0c12cf07
ahg95 <ansgar.glup@gmx.de>
2021-05-12 21:51:46 +0200

    fixed the script

--- Spider game/Assets/SmoothCurveCalculator.cs
+++ Spider game/Assets/SmoothCurveCalculator.cs
@@ -4,44 +4,41 @@ using UnityEngine;
 
 public class SmoothCurveCalculator : MonoBehaviour
 {
+    public List<Transform> PointsToFitCurveTo;
     public float DistanceBetweenCurvePoints;
     public AnimationCurve SmoothingCurve;
+    public CustomLineRenderer customLineRenderer;
 
-    public void SetTransformsToModel(Transform[] transforms)
+    private void Update()
     {
-
+        RenderCurve();
     }
 
-    public Vector3 GetCurvePositionAtInterpolationValue(float t)
+    public void RenderCurve()
     {
-        return Vector3.zero;
-    }
-
-    public Vector3[] CalculateSmoothCurvePoints(Vector3[] curvePoints)
-    {
-        // Contains all calculated curve points in order
-        List<Vector3> smoothCurvePointsList = new List<Vector3>();
-
         // The offset from the startPosition of the currently modeled curve section to the point that should be calculated.
         float curvePointOffset = 0;
 
+        // The index of the next point to render.
+        int pointIndexToRender = 0;
+
         // We loop until i = dataPoints.Length - 1 because the last dataPoint has already been modeled as the endpoint of the curve
-        for (int i = 0; i < curvePoints.Length - 1; i++)
+        for (int i = 0; i < PointsToFitCurveTo.Count - 1; i++)
         {
-            Vector3 startPosition = curvePoints[i];
-            Vector3 endPosition = curvePoints[i + 1];
+            Vector3 startPosition = PointsToFitCurveTo[i].position;
+            Vector3 endPosition = PointsToFitCurveTo[i + 1].position;
 
             Vector3 startDirection;
             if (i == 0)
                 startDirection = endPosition - startPosition;
             else
-                startDirection = endPosition - curvePoints[i - 1];
+                startDirection = endPosition - PointsToFitCurveTo[i - 1].position;
 
             Vector3 endDirection;
-            if (i == curvePoints.Length - 2) // This is true if the endPoint is the position of the last dataPoint
+            if (i == PointsToFitCurveTo.Count - 2) // This is true if the endPoint is the position of the last dataPoint
                 endDirection = endPosition - startPosition;
             else
-                endDirection = curvePoints[i + 2] - startPosition;
+                endDirection = PointsToFitCurveTo[i + 2].position - startPosition;
 
             float startToEndPointDistance = (endPosition - startPosition).magnitude;
 
@@ -49,13 +46,14 @@ public class SmoothCurveCalculator : MonoBehaviour
             {
                 Vector3 smoothCurvePoint = Vector3Utility.GetPointOfSmoothCurveConnectingTwoPoints(startPosition, startDirection, endPosition, endDirection, curvePointOffset / startToEndPointDistance, SmoothingCurve);
 
-                smoothCurvePointsList.Add(smoothCurvePoint);
+                customLineRenderer.SetPosition(pointIndexToRender, smoothCurvePoint);
 
+                pointIndexToRender++;
                 curvePointOffset += DistanceBetweenCurvePoints;
             }
             curvePointOffset -= startToEndPointDistance;
         }
 
-        return smoothCurvePointsList.ToArray();
+        customLineRenderer.TrimLineToNumberOfPoints(pointIndexToRender-1);
     }
 }
