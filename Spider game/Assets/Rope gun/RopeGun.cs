1e255782f977577128edcaaa92d92fcb1777022b
ahg95 <ansgar.glup@gmx.de>
2021-07-28 14:01:37 +0200

    The ropeGun now correctly checks if the object that you look at is close enough and has the right layer to attach a rope to it.

--- Spider game/Assets/Rope gun/RopeGun.cs
+++ Spider game/Assets/Rope gun/RopeGun.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,12 @@ public class RopeGun : MonoBehaviour
     public ChainLinkSource chainLinkSource;
     public Sticky projectilePrefab;
     public Sticky projectile;
+
+
+    [Header("Rope attaching")]
+    public LayerMask raycastObstacles;
+    public LayerMask possibleObjectsToAttachRopeTo;
+    public float maximumDistanceToSurfaceToAttachRopeTo;
 
     RopeGunState gunState;
 
@@ -66,10 +73,40 @@ public class RopeGun : MonoBehaviour
 
     public void PressAttachRopeToSurfaceButton()
     {
-        Debug.Log("Attached rope to surface.");
+        if (gunState == RopeGunState.grappleConnected)
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(muzzle.position, muzzle.forward, out hitInfo, maximumDistanceToSurfaceToAttachRopeTo, raycastObstacles))
+            {
+                GameObject hitObject = hitInfo.collider.gameObject;
+
+                if (LayerMaskContainsLayer(possibleObjectsToAttachRopeTo, hitObject.layer))
+                {
+                    AttachRopeToObjectAtPositionWithNormal(hitObject, hitInfo.point, hitInfo.normal);
+                    InstantiateChainLinkSource();
+
+                    Debug.Log("Attached rope to surface.");
+                }
+            }
+        }
     }
 
+    private bool LayerMaskContainsLayer(LayerMask mask, int layer) => IntegerHasBitSetAtIndex(mask.value, layer);
 
+    // First, we shift the bit that we want to investigate to the rightmost position, which determines if the integer is even or odd. Then we set every other bit to '0' by doing a bitwise AND operation ('&') with the number one,
+    // which has a '0' on all bits except for the rightmost. If this number is equal to one, only then the bit was set.
+    private bool IntegerHasBitSetAtIndex(int integer, int index) => (((integer >> index) & 1) == 1);
+
+
+    private void InstantiateChainLinkSource()
+    {
+        //throw new NotImplementedException();
+    }
+
+    private void AttachRopeToObjectAtPositionWithNormal(GameObject gameObject, Vector3 position, Vector3 direction)
+    {
+        //throw new NotImplementedException();
+    }
 
     private void ShootGrappler()
     {
@@ -89,7 +126,7 @@ public class RopeGun : MonoBehaviour
 
     void SwitchToState(RopeGunState state)
     {
-        switch(state)
+        switch (state)
         {
             case RopeGunState.loaded:
                 SwitchToLoadedState();
