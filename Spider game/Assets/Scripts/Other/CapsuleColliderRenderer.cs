a9040d9a709646c53b57ebcf606bec970e5b8634
ahg95 <ansgar.glup@gmx.de>
2021-08-20 15:49:16 +0200

    Added a capsuleColliderRenderer to the project that is able to render capsuleColliders for the purpose of debugging the VariableLengthChainLink script. Added it to the corresponding prefab.

---
+++ Spider game/Assets/Scripts/Other/CapsuleColliderRenderer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This component renders a capsule collider it is attached to.
+/// </summary>
+[RequireComponent(typeof(CapsuleCollider))]
+public class CapsuleColliderRenderer : MonoBehaviour
+{
+    public Transform topSphere;
+    public Transform cylinder;
+    public Transform bottomSphere;
+
+    CapsuleCollider capsuleCollider;
+
+    CapsuleCollider GetCapsuleCollider()
+    {
+        if (!capsuleCollider)
+            capsuleCollider = GetComponent<CapsuleCollider>();
+
+        return capsuleCollider;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTransformsToMatchCapsuleDimensions();
+    }
+
+    void UpdateTransformsToMatchCapsuleDimensions()
+    {
+        ScaleTransformsToMatchCapsuleDimensions();
+
+        PositionTransformsToMatchCapsuleDimensions();
+    }
+
+    void ScaleTransformsToMatchCapsuleDimensions()
+    {
+        topSphere.localScale = Vector3.one * GetCapsuleCollider().radius * 2;
+        bottomSphere.localScale = Vector3.one * GetCapsuleCollider().radius * 2;
+
+
+        float scaleY = GetCapsuleCollider().height - 2 * GetCapsuleCollider().radius;
+        // The default height of a cylinder is 2 units. Therefore, the cylinder also has to be scaled down in height by half.
+        scaleY *= 0.5f;
+        float scaleXZ = GetCapsuleCollider().radius * 2;
+
+        cylinder.localScale = new Vector3(scaleXZ, scaleY, scaleXZ);
+    }
+
+    void PositionTransformsToMatchCapsuleDimensions()
+    {
+        float sphereDistanceFromOrigin = (GetCapsuleCollider().height - 2 * GetCapsuleCollider().radius) / 2;
+
+        topSphere.localPosition = Vector3.up * sphereDistanceFromOrigin;
+
+        bottomSphere.localPosition = Vector3.down * sphereDistanceFromOrigin;
+
+        cylinder.localPosition = Vector3.zero;
+    }
+}
